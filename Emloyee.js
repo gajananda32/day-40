ce9589f18d5df18a584b169ca5358018d54e6958
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-11-29 16:38:10 +0530

    Added EmployeeAttendence

---
+++ Emloyee.js
@@ -0,0 +1,11 @@
+ //UC 1
+ const IS_ABSENT=0;
+ let empcheck=Math.floor(Math.random()*10)%2;
+ if(empcheck==IS_ABSENT)
+ {
+    console.log("Emloyee is absent");
+ }
+ else
+ {
+     console.log("emloyee is present");
+ }
